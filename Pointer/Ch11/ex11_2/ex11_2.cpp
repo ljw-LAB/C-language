e7db50d4b06175b5d597cd40683282c574a1ad06
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-02-09 22:53:47 +0900

    Ch11 commit  - ex11_2, ex11_3  add

--- Pointer/Ch11/ex11_2/ex11_2.cpp
+++ Pointer/Ch11/ex11_2/ex11_2.cpp
@@ -1,4 +1,8 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <conio.h>
 
 // void main()
 // {
@@ -10,12 +14,89 @@
 //     printf("%lf", d);
 // }
 
+// void main()
+// {
+//     int n1 = 5, n2 =2;
+//     double d;
+
+//     d = (double) n1 / n2;
+
+//     printf("%lf", d);
+// }
+
+// void main()
+// {
+//     int n1, n2;
+//     double d;
+
+//     printf("두 정수를 입력하세요 : ");
+//     scanf("%d %d", &n1, &n2);
+//     if(n2)
+//         d = (double) n1 / n2;
+//     else
+//     {
+//         puts("n2는 0이 될 수 없습니다 \n");
+//         return;
+//     }
+//     printf("%lf", d);
+// }
+
+// void menu()
+// {
+//     puts("=================");
+//     puts("1. 두 정수의 덧셈");
+//     puts("2. 두 정수의 뺄셈");
+//     puts("3. 두 정수의 곱셈");
+//     puts("4. 두 정수의 나눗셈");
+
+// }
+
+// void main()
+// {
+//     int n1, n2;
+
+//     printf("두 정수를 입력하세요 : \n");
+//     scanf("%d %d", &n1, &n2);
+    
+//     menu();
+//     switch (_getch())
+//        {
+//         case '1':
+//             printf("%d + %d = %d", n1, n2, n1 + n2);
+//             break;
+//         case '2':
+//             printf("%d - %d = %d", n1, n2, n1 - n2);
+//             break;
+//         case '3':
+//             printf("%d * %d = %d", n1, n2, n1 * n2);
+//             break;
+//         case '4':
+//             if (n2)
+//                 printf("%d / %d = %g", n1, n2, (double)n1 / n2);
+//             else
+//                 puts("분모가 0입니다.");
+//             break;
+//         default :
+//             puts("1~4까지 입력 가능합니다");
+//     }
+// }
+
+// void main()
+// {
+//     int i;
+
+//     for(i=0; i< 100; i++)
+//     {
+//         printf("%2d", i/50);
+//     }
+// }
+
 void main()
 {
-    int n1 = 5, n2 =2;
-    double d;
+    int i;
 
-    d = (double) n1 / n2;
-
-    printf("%lf", d);
+    for(i=0; i< 100; i++)
+    {
+        printf("%2d", i/10);
+    }
 }
---
+++ Pointer/Ch11/ex11_3/ex11_3.cpp
@@ -0,0 +1,114 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// void main()
+// {
+//     int i;
+//     for (i = 0; i < 10; i++)
+//         printf("%d \n", rand());
+// }
+
+// void main()
+// {
+//     int i;
+//     srand(1);//srand(2);srand(3);
+//     for (i = 0; i < 10; i++)
+//         printf("%d \n", rand());
+// }
+
+// void main()
+// {
+//     int i;
+//     srand((unsigned)time(NULL));
+//     for (i = 0; i < 10; i++)
+//     printf("%d \n", rand());
+// }
+
+// void main()
+// {
+//     int i;
+//     for (i = 0; i < 10; i++)
+//     {
+//         srand((unsigned)time(NULL));
+//         printf("%d \n", rand());
+//     }
+// }
+
+// void main()
+// {
+//     int i;
+
+//     srand( (unsigned)time(NULL));
+//     for (i = 0; i < 10; i++)
+//         printf("%d \n", rand()%10);
+
+// }
+
+// void main()
+// {
+//     int i, n;
+
+//     srand( (unsigned)time(NULL));
+//     for (i = 0; i < 10; i++)
+//     {
+//         n = rand()%26 + 65;
+//         printf("%c %d\n", n, n);
+//     }
+
+// }
+
+// void main()
+// {
+//     int i;
+
+//     for(i=0; i<100;i++)
+//         if( i % 10 == 0)
+//             printf("%d ", i);
+// }
+
+// void main()
+// {
+//     int i;
+//     int arr[100];
+
+//     srand( (unsigned)time(NULL));
+    
+//     for(i = 0 ; i < 100; i++)
+//     {
+//         arr[i] = rand()%100;
+//     }
+//     for(i = 0 ; i < 100; i++)
+//     {
+//         printf("%3d", arr[i]);
+//         if( (i+1) % 10 ==0)
+//             puts("");
+//     }
+
+// }
+
+void main()
+{
+    int arr[100];
+    int numCount[10] ={0};
+    int i;
+    for(i = 0 ; i < 100; i++)
+    {
+        arr[i] = rand()%100;
+    }
+    for(i = 0 ; i < 100; i++)
+    {
+        printf("%3d", arr[i]);
+        if( (i+1) % 10 ==0)
+            puts("");
+    }
+    puts("=====================");
+    for(i = 0 ; i < 100; i++)
+    {
+        numCount[arr[i]/10]++;
+    }
+    for(i = 0 ; i < 10; i++)
+    {
+        printf("%d ~ %d 까지의 정수는 %d개 입니다. \n", i*10, (i+1)*10-1, numCount[i]);
+    }
+}
